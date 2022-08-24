66eadfe97846002b5371e095f255608d7bbc07ab
Emir Tüfekci <emirtufekci4537@gmail.com>
2022-08-24 14:48:04 +0300

    clear db file

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true });
